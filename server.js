7a7e095e6129cefd6e724cf3139f9ec4fd00e88b
Sandeep <humanityisoverrrated997@gmail.com>
2022-06-02 15:26:04 +0545

    env

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6pkgt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
-    server.listen(5000);
+    server.listen(process.env.PORT||5000);
     console.log("connected");
   })
   .catch((error) => console.log("error", error));
